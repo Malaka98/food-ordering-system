299862224fb8a87842f8b108a393d483a93abfd4
Malaka98 <malaka.aruna14413@gmail.com>
2023-01-17 03:28:03 +0530

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(4000, () => {
+    console.log('Server listening on port 3000!');
+});
