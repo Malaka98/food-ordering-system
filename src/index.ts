9446787113c0ccf004c360cfd74c46ee7c623d72
Malaka98 <malaka.aruna14413@gmail.com>
2023-02-05 03:04:33 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,10 @@ import FoodModule from "./foodModule/foodModule";
 
 config();
 const corsOptions = {
-  origin: "http://localhost:4200",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 200,
+    origin: process.env.ORIGIN_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 200,
 };
 
 export const app = express();
@@ -34,17 +34,17 @@ container.load(UserModule, FoodModule);
 
 const server = new InversifyExpressServer(container, null, {rootPath: "/api"});
 server.setConfig((app) => {
-  // add your express middlewares here
-  // add body parser
-  app.use(bodyParser.urlencoded({
-    extended: true,
-  }));
-  app.use(bodyParser.json());
+    // add your express middlewares here
+    // add body parser
+    app.use(bodyParser.urlencoded({
+        extended: true,
+    }));
+    app.use(bodyParser.json());
 });
 app.use(server.build());
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}!`);
-  logger.info(`Server listening on port ${PORT}!`);
+    console.log(`Server listening on port ${PORT}!`);
+    logger.info(`Server listening on port ${PORT}!`);
 });
