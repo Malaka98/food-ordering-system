44d6361b9193886dc38590c2db70a2520eff08d7
Malaka98 <malaka.aruna14413@gmail.com>
2023-02-06 01:34:47 +0530

    fix

--- test/user.test.ts
+++ test/user.test.ts
@@ -4,87 +4,88 @@ import {app} from "../src";
 
 describe("User login and singUp test", () => {
 
-  beforeEach(async () => {
-    try {
-      const response = await request(app)
-        .post("/api/user")
-        .send({
-          firstName: "Malaka",
-          lastName: "jayakodi",
-          username: "jayakodi",
-          email: "jayakodi@gmail.com",
-          address: "colombo",
-          password: "123456",
-          phoneNumber: "07771234123",
-        })
-        .set("Accept", "application/json")
-        .expect(200);
-      console.log(response.body);
-    } catch (e) {
-      expect(e).toBeFalsy();
-    }
-  });
-
-  it("user login", function (done) {
-    request(app)
-      .post("/api/user/login")
-      .send({username: "rootx", password: "123456"})
-      .set("Accept", "application/json")
-      .expect(200)
-      .end(function (err, res) {
-        if (err) {
-          return done(err);
+    beforeEach(async () => {
+        try {
+            const response = await request(app)
+                .post("/api/user")
+                .send({
+                    firstName: "Malaka",
+                    lastName: "jayakodi",
+                    username: "jayakodi",
+                    email: "jayakodi@gmail.com",
+                    address: "colombo",
+                    password: "123456",
+                    phoneNumber: "07771234123",
+                })
+                .set("Accept", "application/json")
+                .expect(200);
+            console.log(response.body);
+        } catch (e) {
+            console.log(e.message)
+            expect(e).toBeFalsy();
         }
-        console.log(res.headers["set-cookie"]);
-        return done();
-      });
-  });
+    });
 
-  afterAll(async () => {
-    try {
-      const response = await request(app)
-        .delete("/api/user/aruna@gmail.com")
-        .set("Accept", "application/json")
-        .expect(200);
-      console.log(response.body);
-    } catch (e) {
-      expect(e).toBeFalsy();
-    }
-  });
+    it("user login", function (done) {
+        request(app)
+            .post("/api/user/login")
+            .send({username: "rootx", password: "123456"})
+            .set("Accept", "application/json")
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                console.log(res.headers["set-cookie"]);
+                return done();
+            });
+    });
+
+    afterAll(async () => {
+        try {
+            const response = await request(app)
+                .delete("/api/user/aruna@gmail.com")
+                .set("Accept", "application/json")
+                .expect(200);
+            console.log(response.body);
+        } catch (e) {
+            expect(e).toBeFalsy();
+        }
+    });
 
 });
 
 describe("User API Testing", () => {
 
-  let cookie: any;
+    let cookie: any;
 
-  beforeAll(async () => {
-    try {
-      const response = await request(app)
-        .post("/api/user/login")
-        .send({username: "rootx", password: "123456"})
-        .set("Accept", "application/json")
-        .expect(200);
-      cookie = response.headers["set-cookie"];
-    } catch (e) {
-      expect(e).toBeFalsy();
-    }
-  });
-
-  it("Get user by id API test", (done) => {
-    const id = "63d7e3e65dfb150d5b552aa2";
-
-    request(app)
-      .get(`/api/user/${id}`)
-      .set("cookie", cookie)
-      .expect("Content-Type", /json/)
-      .expect(200)
-      .end(function (err, res) {
-        if (err) {
-          return done(err);
+    beforeAll(async () => {
+        try {
+            const response = await request(app)
+                .post("/api/user/login")
+                .send({username: "rootx", password: "123456"})
+                .set("Accept", "application/json")
+                .expect(200);
+            cookie = response.headers["set-cookie"];
+        } catch (e) {
+            expect(e).toBeFalsy();
         }
-        console.log(res.body);
-        return done();
-      });
-  });
+    });
+
+    it("Get user by id API test", (done) => {
+        const id = "63d7e3e65dfb150d5b552aa2";
+
+        request(app)
+            .get(`/api/user/${id}`)
+            .set("cookie", cookie)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+                console.log(res.body);
+                return done();
+            });
+    });
 });
